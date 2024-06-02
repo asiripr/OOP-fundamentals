eb0292e837d9843470668e176ac3a5318bdf06bf
Asiri Pramodaya <asiripramoday2000@gmail.com>
2024-06-02 18:13:56 +0530

    added some modifications on enums

--- src/learnEnum/LearnEnum.java
+++ src/learnEnum/LearnEnum.java
@@ -1,5 +1,21 @@
 package learnEnum;
 
-public class LearnEnum {
+import java.util.Iterator;
 
+enum Days{ // enum is a group of constants
+	MONDAY, // every constant should be written in capital letters
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
+public class LearnEnum {
+	public static void main(String[] args) {
+		for (Days myDay : Days.values()) { // Days.values() returns a list of constants
+			System.out.println(myDay);
+			
+		}
+	}
 }
